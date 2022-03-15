3a0d1a83cba29c78f00dc40126dc07d309a807c4
Javier <javiersch705@gmail.com>
2022-03-15 06:37:04 -0300

    Add Hat

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,66 @@
-import { Application, Loader, Sprite } from 'pixi.js'
+import { Application, Container, Loader, Sprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	width: 1280,
+	height: 720
 });
 
-Loader.shared.add({url: "./gollum.png", name: "MySmeagol"})
-Loader.shared.add({url: "./clampy.png", name: "Clampy"})
+window.addEventListener("resize", ()=>{
+	const scaleX = window.innerWidth / app.screen.width;
+	const scaleY = window.innerHeight / app.screen.height;
+	const scale = Math.min(scaleX, scaleY);
+	
+	const gameWidth = Math.round(app.screen.width * scale);
+	const gameHeight = Math.round(app.screen.height * scale);
+
+	const marginHorizontal = Math.floor((window.innerWidth - gameWidth) / 2);
+	const marginVertical = Math.floor((window.innerHeight - gameHeight) / 2);
+	
+	app.view.style.width = gameWidth + "px";
+	app.view.style.height = gameHeight + "px";
+
+	app.view.style.marginLeft = marginHorizontal + "px";
+	app.view.style.marginRight = marginVertical + "px";
+
+	app.view.style.marginTop = marginVertical + "px";
+	app.view.style.marginBottom = marginVertical + "px";
+})
+window.dispatchEvent(new Event("resize"));
+
+Loader.shared.add({url: "./gollum.png", name: "Gollum"})
+Loader.shared.add({url: "./santahat.png", name: "Hat"})
+//Loader.shared.add({url: "./clampy.png", name: "Clampy"});
 
 Loader.shared.onComplete.add(()=>{
-	const clampy: Sprite = Sprite.from("MySmeagol");
+	const gollum: Sprite = Sprite.from("Gollum");
+	//console.log("Hola World!", clampy.width, clampy.height);
 
-	console.log("Hola World!", clampy.width, clampy.height);
+	//gollum.position.set(200 , 150);
+	//gollum.scale.set(1 , 1)
+	
+	const hat: Sprite = Sprite.from("Hat");
 
-	//clampy.anchor.set(0.5);
+	//hat.scale.set(0.5, 0.5);
+	//hat.position.set(230, 145);
 
-	clampy.x = 0;
-	clampy.y = 0;
+	const gollumWithHat: Container = new Container();
 
-	app.stage.addChild(clampy);
+	gollumWithHat.addChild(gollum);
+	gollumWithHat.addChild(hat);
+
+	gollumWithHat.scale.set(1);
+
+	gollumWithHat.x = 200;
+	gollumWithHat.y = 150;
+
+	app.stage.addChild(gollumWithHat);
+
+	//gollum.x = gollum.x + 200;
+	//hat.x = hat.x + 200;
 
 })
 
